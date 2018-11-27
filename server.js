f2dc38fe2f869e678cc6df778665f4f58475af11
Andre Aditya P <andre02.9d@gmail.com>
2018-11-27 16:23:51 +0700

    Merapikan source code

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = express()
 var bodyParser = require('body-parser')
 var router = express.Router();
 var port = process.env.PORT || 3000
-//== SETTING // DB
+//===== SETTING DATABASE ====
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/coba')
 var user = require('./models/user')
-//== KONFIG BODY parser
+
+//===== KONFIG BODY PARSER ====
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-//middleware
+//===== MIDDLEWARE ====
 
 //TEST Router
 router.get('/',function(req,res){
@@ -19,56 +20,60 @@ router.get('/',function(req,res){
   })
 })
 
+//===== METHOD POST ====
 router.route('/user')
-  .post(function(req,res){
+.post(function(req,res){
   var User = new user()
   User.nama = req.body.nama
   User.pass = req.body.pass
-
   User.save(function(err){
     if(err) res.send(err)
-
     res.json({message: "user berhasil di input"})
   })
-}).get(function(req,res){
+})
+.get(function(req,res){
   user.find(function(err,peserta){
     if(err) res.send(err)
-      res.json(peserta)
+    res.json(peserta)
   })
 })
-
+//===== METHOD GET ====
 router.route('/user/:nama')
-  .get(function(req,res){
-    user.find({nama:req.params.nama},function(err,peserta){
-      if(err) res.send(peserta)
-      res.json(peserta)
+.get(function(req,res){
+  user.find({nama:req.params.nama},function(err,peserta){
+    if(err) res.send(peserta)
+    res.json(peserta)
+  })
+})
+//===== METHOD UPDATE ====
+.put(function(req,res){
+  user.update(
+    {nama:req.params.nama},
+    {nama:req.body.nama},
+    function(err,peserta){
+      if(err) res.send(err)
+      res.json("user berhasil di update")
     })
-  }) .put(function(req,res){
-      user.update(
-        {nama:req.params.nama},
-        {nama:req.body.nama},
-        function(err,peserta){
-        if(err) res.send(err)
-        res.json("user berhasil di update")
-      })
-    }).delete(function(req,res){
-      user.remove({
-        nama: req.params.nama
-      },function(err, user){
-        if(err) res.send(err)
-        res.json({message:"user berhasil dihapus"})
-      })
+  })
+  //===== METHOD DELETE ====
+  .delete(function(req,res){
+    user.remove({
+      nama: req.params.nama
+    },function(err, user){
+      if(err) res.send(err)
+      res.json({message:"user berhasil dihapus"})
     })
+  })
 
 
-//== PREFIX API
-app.use('/api', router)
-//app.get('/',function(req,res){
-//  res.send('haloo')
-//})
-//app.get('/user/:nama',function(req, res){
-//  res.send("namanya adalah " + req.params.nama)
-//})
+  //== PREFIX API
+  app.use('/api', router)
+  //app.get('/',function(req,res){
+  //  res.send('haloo')
+  //})
+  //app.get('/user/:nama',function(req, res){
+  //  res.send("namanya adalah " + req.params.nama)
+  //})
 
-app.listen(port)
-console.log('magic happen on port' + port)
+  app.listen(port)
+  console.log('magic happen on port' + port)
